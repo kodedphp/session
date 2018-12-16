ee3a50e02c8c4868a2cf19d02d53e8e51f3fbf8f
Mihail Binev <kodeart@gmail.com>
2018-12-16 06:45:00 +0100

    - removes the session instance from request object - uses camel-case for attribute names

--- SessionMiddleware.php
+++ SessionMiddleware.php
@@ -19,35 +19,25 @@ use Psr\Http\Server\{MiddlewareInterface, RequestHandlerInterface};
 
 class SessionMiddleware implements MiddlewareInterface
 {
-
-    public const SESSION_NAME    = 'session';
-    public const SESSION_STARTED = 'session_started';
+    public const SESSION_STARTED = 'sessionStarted';
 
     /**
      * @var array Options for session_start()
      * @see http://php.net/manual/en/session.configuration.php
      */
     private $options = [];
-    private $class;
 
     public function __construct(ConfigurationFactory $settings)
     {
         $this->options = session_register_custom_handler($settings)->sessionParameters();
-        $this->class   = $settings->get('class', PhpSession::class);
-
-        if (false === is_a($this->class, Session::class, true)) {
-            throw SessionException::forInvalidClass($this->class);
-        }
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         session_write_close();
 
-        if (PHP_SESSION_ACTIVE !== session_status() /* && 'cli' !== php_sapi_name() */) {
-            $request = $request
-                ->withAttribute(self::SESSION_STARTED, session_start($this->options))
-                ->withAttribute(self::SESSION_NAME, new $this->class);
+        if (PHP_SESSION_ACTIVE !== session_status()) {
+            $request = $request->withAttribute(self::SESSION_STARTED, $this->startSession());
         }
 
         $response = $handler->handle($request);
@@ -57,5 +47,14 @@ class SessionMiddleware implements MiddlewareInterface
         }
 
         return $response;
+    }
+
+    private function startSession(): bool
+    {
+        if ($started = session_start($this->options)) {
+            new PhpSession;
+        }
+
+        return $started;
     }
 }
