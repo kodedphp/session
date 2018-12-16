20169433c89224ca999f7288e58467f09efc7521
Mihail Binev <kodeart@gmail.com>
2018-12-16 06:41:51 +0100

    - renames the class - uses camel-case attribute names

---
+++ SessionAuthenticatedMiddleware.php
@@ -0,0 +1,55 @@
+<?php
+
+/*
+ * This file is part of the Koded package.
+ *
+ * (c) Mihail Binev <mihail@kodeart.com>
+ *
+ * Please view the LICENSE distributed with this source code
+ * for the full copyright and license information.
+ *
+ */
+
+namespace Koded\Session;
+
+use Koded\Http\{StatusCode, ServerResponse};
+use Koded\Stdlib\Interfaces\ConfigurationFactory;
+use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
+use Psr\Http\Server\{MiddlewareInterface, RequestHandlerInterface};
+
+
+class SessionAuthenticatedMiddleware implements MiddlewareInterface
+{
+    public const AUTHENTICATED = 'authenticated';
+    public const LOGIN_URI   = 'loginUri';
+
+    private $redirectTo = '/';
+
+    public function __construct(ConfigurationFactory $settings)
+    {
+        $this->redirectTo = $settings->get(self::LOGIN_URI, $this->redirectTo);
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (true !== ($_SESSION[self::AUTHENTICATED] ?? false)) {
+            return $this->redirect();
+        }
+
+        return $handler->handle($request);
+    }
+
+    private function redirect(): ResponseInterface
+    {
+        // Ajax requests should be handled in the browser
+        if (strtoupper($_SERVER['HTTP_X_REQUESTED_WITH'] ?? '') === 'XMLHTTPREQUEST') {
+            return (new ServerResponse(json_encode([
+                'location' => $this->redirectTo,
+                'status'   => StatusCode::UNAUTHORIZED
+            ]), StatusCode::UNAUTHORIZED));
+        }
+
+        return (new ServerResponse(null, StatusCode::TEMPORARY_REDIRECT))
+            ->withHeader('Location', $this->redirectTo);
+    }
+}
