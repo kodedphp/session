62e3f48899b95fb7d250a28bd2216137bc67192b
Mihail Binev <kodeart@gmail.com>
2020-09-22 12:37:37 +0200

    updates

--- SessionAuthenticatedMiddleware.php
+++ SessionAuthenticatedMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Koded package.
  *
@@ -7,17 +6,14 @@
  *
  * Please view the LICENSE distributed with this source code
  * for the full copyright and license information.
- *
  */
-
 namespace Koded\Session;
 
+use Koded\Stdlib\Configuration;
 use Koded\Http\{ServerResponse, StatusCode};
-use Koded\Stdlib\Interfaces\ConfigurationFactory;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
 use Psr\Http\Server\{MiddlewareInterface, RequestHandlerInterface};
 use function Koded\Stdlib\json_serialize;
-
 
 class SessionAuthenticatedMiddleware implements MiddlewareInterface
 {
@@ -26,7 +22,7 @@ class SessionAuthenticatedMiddleware implements MiddlewareInterface
 
     private $redirectTo = '/';
 
-    public function __construct(ConfigurationFactory $settings)
+    public function __construct(Configuration $settings)
     {
         $this->redirectTo = $settings->get(self::LOGIN_URI, $this->redirectTo);
     }
@@ -36,7 +32,6 @@ class SessionAuthenticatedMiddleware implements MiddlewareInterface
         if (true === ($_SESSION[self::AUTHENTICATED] ?? false)) {
             return $handler->handle($request);
         }
-
         // Ajax requests should be handled in the browser
         if ('XMLHTTPREQUEST' === strtoupper($_SERVER['HTTP_X_REQUESTED_WITH'] ?? '')) {
             return (new ServerResponse(json_serialize([
@@ -44,7 +39,6 @@ class SessionAuthenticatedMiddleware implements MiddlewareInterface
                 'status'   => StatusCode::UNAUTHORIZED
             ]), StatusCode::UNAUTHORIZED));
         }
-
         return (new ServerResponse(null, StatusCode::TEMPORARY_REDIRECT))
             ->withHeader('Location', $this->redirectTo);
     }
