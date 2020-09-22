50e8b3e9dcdf3814e553842f882de1fcbd82994c
Mihail Binev <kodeart@gmail.com>
2020-09-22 12:40:05 +0200

    WIP: - start session in the process() method - save the session and start again

--- SessionMiddleware.php
+++ SessionMiddleware.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
  * This file is part of the Koded package.
  *
@@ -7,33 +6,38 @@
  *
  * Please view the LICENSE distributed with this source code
  * for the full copyright and license information.
- *
  */
-
 namespace Koded\Session;
 
-use Koded\Stdlib\Interfaces\ConfigurationFactory;
+use Koded\Http\StatusCode;
+use Koded\Stdlib\Configuration;
 use Psr\Http\Message\{ResponseInterface, ServerRequestInterface};
 use Psr\Http\Server\{MiddlewareInterface, RequestHandlerInterface};
-
 
 class SessionMiddleware implements MiddlewareInterface
 {
     public const SESSION_STARTED = 'sessionStarted';
+    public const SESSION_EXPIRE_IN = 'X-Session-ExpireIn';
 
-    public function __construct(ConfigurationFactory $settings)
+    private array $options;
+
+    public function __construct(Configuration $settings)
     {
-        session_start(session_register_custom_handler($settings)->sessionParameters());
+        $this->options = session_register_custom_handler($settings)->sessionParameters();
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $request  = $request->withAttribute(self::SESSION_STARTED, PHP_SESSION_ACTIVE === session_status());
+        session_start($this->options);
+        $request = $request->withAttribute(self::SESSION_STARTED, PHP_SESSION_ACTIVE === session_status());
+
         $response = $handler->handle($request);
 
-        if (500 !== $response->getStatusCode()) {
+//        $expireIn = $response->getHeaderLine(self::SESSION_EXPIRE_IN);
+//        if ($response->getStatusCode() < StatusCode::INTERNAL_SERVER_ERROR) {
             session_write_close();
-        }
+            session_start();
+//        }
 
         return $response;
     }
