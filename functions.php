aafeddeb0fb494ba8d422cca676d589e2469133c
Mihail Binev <kodeart@gmail.com>
2020-09-22 12:26:42 +0200

    - set the cookie params before the handler creation

--- functions.php
+++ functions.php
@@ -1,5 +1,4 @@
 <?php declare(strict_types=1);
-
 /*
  * This file is part of the Koded package.
  *
@@ -7,12 +6,10 @@
  *
  * Please view the LICENSE distributed with this source code
  * for the full copyright and license information.
- *
  */
-
 namespace Koded\Session;
 
-use Koded\Stdlib\Interfaces\ConfigurationFactory;
+use Koded\Stdlib\Configuration;
 use SessionHandlerInterface;
 
 /**
@@ -26,30 +23,27 @@ use SessionHandlerInterface;
 function session(Session $instance = null): Session
 {
     static $session;
-
     if ($instance) {
         return $session = $instance;
     }
-
-    return $session ?? $session = new PhpSession;
+    return $session ??= new PhpSession;
 }
 
 /**
  * Creates and registers a new session handler.
  *
- * @param ConfigurationFactory $configuration
+ * @param Configuration $configuration
  *
  * @return SessionConfiguration The session configuration instance
  * @throws SessionException
  */
-function session_register_custom_handler(ConfigurationFactory $configuration): SessionConfiguration
+function session_register_custom_handler(Configuration $configuration): SessionConfiguration
 {
     $configuration = new SessionConfiguration($configuration);
-
     if (PHP_SESSION_ACTIVE !== session_status()) {
+        session_set_cookie_params($configuration->cookieParameters());
         session_set_save_handler(session_create_custom_handler($configuration), false);
     }
-
     return $configuration;
 }
 
@@ -63,10 +57,8 @@ function session_register_custom_handler(ConfigurationFactory $configuration): S
 function session_create_custom_handler(SessionConfiguration $configuration): SessionHandlerInterface
 {
     $handler = __NAMESPACE__ . '\\Handler\\' . ucfirst($configuration->handler()) . 'Handler';
-
     if (false === class_exists($handler, true)) {
-        throw SessionException::forNotFoundHandler($handler);
+        throw SessionException::forHandlerNotFound($handler);
     }
-
     return new $handler($configuration);
 }
